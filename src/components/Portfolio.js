3bf928d948657fbc602dc0b858a091a72447eb46
Jay Cohan <jamiecohan@gmail.com>
2022-01-25 23:45:55 +0000

    Updated portfolio, added slider

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import netflix from "../images/netflix.jpeg";
-import cityGuide from "../images/city-guide-app.png";
-import mernBlog from "../images/mern-blog.png";
-import portfolio from "../images/portfolio.png";
-import taskManager from "../images/task-manager.jpg";
 // FONTAWESOME IMPORTS
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 // REACT POPUPBOX
 import { PopupboxManager, PopupboxContainer } from "react-popupbox";
 import "react-popupbox/dist/react-popupbox.css";
-// AVATARS IMPORTS
-import avatar1 from "../images/avatars/avatar-1.png";
-import avatar2 from "../images/avatars/avatar-2.png";
-import avatar3 from "../images/avatars/avatar-3.png";
-import avatar4 from "../images/avatars/avatar-4.png";
-import { Carousel } from "react-responsive-carousel";
+// ICON IMPORTS
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import PocketCoachCarousel from "./PocketCoachCarousel";
-//import Carousel from 'react-bootstrap/Carousel'
 import { FaGithub } from 'react-icons/fa';
 import { FaLink } from 'react-icons/fa';
 import { FaInfo } from 'react-icons/fa';
-import { FaAngleLeft } from 'react-icons/fa';
-import { FaAngleRight } from 'react-icons/fa';
+//Image slider
+import ImageSlider from "./ImageSlider";
+import { SliderData } from './SliderData';
+import ImageSliderTC from "./ImageSliderTC";
+import { SliderDataTC } from './SliderDataTC';
+
 
 const Portfolio = () => {
   // TC Events
@@ -67,40 +57,19 @@ const Portfolio = () => {
     fadeIn: true,
     fadeInSpeed: 500
   }
-
-//   export const handlers = () => (
-//     <Carousel onClickThumb={action('click thumb')} onClickItem={action('click item')} onChange={action('change')}>
-//         {baseChildren.props.children}
-//     </Carousel>
-// );
-
-// export const withCustomStatusArrowsAndIndicators = () => {
-//     const arrowStyles = {
-//         position: 'absolute',
-//         zIndex: 2,
-//         top: 'calc(50% - 15px)',
-//         width: 30,
-//         height: 30,
-//         cursor: 'pointer',
-//     };
-
-//     const indicatorStyles = {
-//         background: '#fff',
-//         width: 8,
-//         height: 8,
-//         display: 'inline-block',
-//         margin: '0 8px',
-//     };
-
-//   };
-
+  
 const arrowStyles  = {
   position: 'absolute',
   zIndex: 2,
   top: 'calc(50% - 15px)',
-  width: 30,
-  height: 30,
+  width: 40,
+  height: 40,
   cursor: 'pointer',
+  backgroundColor: '#666666',
+  color: 'white',
+  fontSize: '25px',
+border: 'none',
+borderRadius: "50%"
 };
 
 const indicatorStyles  = {
@@ -116,68 +85,10 @@ const indicatorStyles  = {
       <div className="container">
       <h1 className="text-uppercase text-center py-5">portfolio</h1>
         <div class="row">
-          <div class="col-12 col-md-6 col-lg-6" >
+          <div class="col-12 col-md-12 col-lg-12" >
             <div class="portfolioCard">
               <div className="cardCarousel">
-                <Carousel
-                  showArrows={true}
-                  infiniteLoop={true}
-                  showThumbs={false}
-                  showStatus={false}
-                  autoPlay={false}
-                  interval={3000}
-                  renderArrowPrev={(onClickHandler, hasPrev, label) =>
-                    hasPrev && (
-                        <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
-                            <FaAngleLeft></FaAngleLeft>
-                        </button>
-                    )
-                }
-                renderArrowNext={(onClickHandler, hasNext, label) =>
-                    hasNext && (
-                        <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, right: 15 }}>
-                            <FaAngleRight></FaAngleRight>
-                        </button>
-                    )
-                }
-                renderIndicator={(onClickHandler, isSelected, index, label) => {
-                  if (isSelected) {
-                      return (
-                          <li
-                              style={{ ...indicatorStyles, background: '#000' }}
-                              aria-label={`Selected: ${label} ${index + 1}`}
-                              title={`Selected: ${label} ${index + 1}`}
-                          />
-                      );
-                  }
-                  return (
-                      <li
-                          style={indicatorStyles}
-                          onClick={onClickHandler}
-                          onKeyDown={onClickHandler}
-                          value={index}
-                          key={index}
-                          role="button"
-                          tabIndex={0}
-                          title={`${label} ${index + 1}`}
-                          aria-label={`${label} ${index + 1}`}
-                      />
-                  );
-              }}
-                >
-                  <>
-                    <img src={avatar1} alt="John Doe 1" />
-                  </>
-                  <>
-                    <img src={avatar2} alt="Jane Doe 2" />
-                  </>
-                  <>
-                    <img src={avatar3} alt="John Doe 3" />
-                  </>
-                  <>
-                    <img src={avatar4} alt="John Doe 4" />
-                  </>
-                </Carousel>
+              <ImageSlider slides={SliderData} />
               </div>
               <div class="card-body">
                 <h5 class="card-title">Pocket Coach</h5>
@@ -191,48 +102,14 @@ const indicatorStyles  = {
               </div>
             </div>
           </div>
-          <div class="col-12 col-md-6 col-lg-6">
+          <div class="col-12 col-md-12 col-lg-12">
             <div class="portfolioCard">
             <div className="cardCarousel">
-                <Carousel
-                  showArrows={true}
-                  infiniteLoop={true}
-                  showThumbs={false}
-                  showStatus={false}
-                  autoPlay={false}
-                  interval={3000}
-                  renderArrowPrev={(onClickHandler, hasPrev, label) =>
-                    hasPrev && (
-                        <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
-                            <FaAngleLeft></FaAngleLeft>
-                        </button>
-                    )
-                }
-                renderArrowNext={(onClickHandler, hasNext, label) =>
-                    hasNext && (
-                        <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, right: 15 }}>
-                            <FaAngleRight></FaAngleRight>
-                        </button>
-                    )
-                }
-                >
-                  <>
-                    <img src={avatar1} alt="John Doe 1" />
-                  </>
-                  <>
-                    <img src={avatar2} alt="Jane Doe 2" />
-                  </>
-                  <>
-                    <img src={avatar3} alt="John Doe 3" />
-                  </>
-                  <>
-                    <img src={avatar4} alt="John Doe 4" />
-                  </>
-                </Carousel>
+            <ImageSliderTC slides={SliderDataTC} />
               </div>
               <div class="card-body">
                 <h5 class="card-title">TC Events</h5>
-                <p>A web application called TC Events that sells tickets for various events.</p>
+                <p>A Ticket Selling App</p>
                 <b><FaInfo></FaInfo></b> <a className="hyper-link" onClick={openPopupboxTCEvents}>Tech Stack & Functionaility</a>
                 <div id='links'>
                 <button class="button button4" onClick={() => window.open("https://portfolio-rea-and-material-ui.herokuapp.com/", "_blank")}><b><FaLink></FaLink> Demo</b></button>
---
+++ src/components/SliderDataTC.js
@@ -0,0 +1,38 @@
+import tc from "../images/tc.PNG";
+import tc_1 from "../images/tc_1.jpg";
+import tc_2 from "../images/tc_2.png";
+import tc_4 from "../images/tc_4.png";
+import tc0 from "../images/tc0.PNG";
+import tc1 from "../images/tc1.PNG";
+import tc2 from "../images/tc2.PNG";
+
+
+export const SliderDataTC = [
+    {
+      image: tc
+    },
+    {
+      image: tc_1
+        
+    },
+    {
+      image: tc_2
+        
+    },
+    {
+      image: tc_4
+        
+    },
+    {
+      image: tc0
+        
+    },
+    {
+      image: tc1
+        
+    },
+    {
+      image: tc2
+        
+    }
+  ];
